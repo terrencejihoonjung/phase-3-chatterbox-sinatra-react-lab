7a7429d815cb4a5f49fc7d1d1a3099b66a1b5d82
--replace-all <terrencejung928@gmail.com>
2022-07-11 02:32:43 -0700

    commit tree e17940654eb79c6ba0ffe2a5348b116ef280ffac

--- chatterbox-server/app/controllers/application_controller.rb
+++ chatterbox-server/app/controllers/application_controller.rb
@@ -2,5 +2,30 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add routes
+
+  get '/messages' do
+    messages = Message.all.order(:created_at)
+    messages.to_json
+  end
+
+  post '/messages' do
+    new_message = Message.create(
+      username: params[:username],
+      body: params[:body]
+    )
+    new_message.to_json
+  end
+
+  patch '/messages/:id' do
+    update_message = Message.all.find(params[:id])
+    update_message.update(body: params[:body])
+    update_message.to_json
+  end
+
+  delete '/messages/:id' do
+    delete_message = Message.all.find(params[:id])
+    delete_message.destroy
+    delete_message.to_json
+  end
   
 end
